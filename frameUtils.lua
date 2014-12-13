ee79466e7fc6d96459bf497b53cded89da09a584
Elijah Andrews <norman.reggox@gmail.com>
2014-12-13 20:02:33 +0000

    Continued development of the UI, specifically the Add Mob part.

--- frameUtils.lua
+++ frameUtils.lua
@@ -1,4 +1,9 @@
-function newFrame(title,width,height)
+function round(x)
+  return x>=0 and math.floor(x+0.5) or math.ceil(x-0.5)
+end
+
+function newFrame(title,width,buttons)
+local height = (buttons * 38) + 28
 -- The main frame.
 local frame = CreateFrame("Frame","pane",UIParent,"BasicFrameTemplate")
 frame:SetPoint("CENTER",0,0)
@@ -17,10 +22,10 @@ region:SetParent(frame)
 -- The title text.
 local text = CreateFrame("Frame","title",frame)
 text:SetHeight(50)
-text:SetWidth(50)
+text:SetWidth(width-30)
 text.text = text:CreateFontString(nil,nil, "GameFontNormal")
 text.text:SetAllPoints()
-text:SetPoint("CENTER", -15 , (height/2 - 12))
+text:SetPoint("CENTER", -15 , ((height/2) - 12))
 
 -- Sets the text for the title text.
 function textMessage(title)
@@ -32,14 +37,96 @@ return frame
 end
 
 -- Creates a new button.
-function newButton(text, width, height, parent, onClick)
+function newButton(text, width, parent, onClick, buttonNo)
 local button = CreateFrame("Button",nil,parent,"UIPanelButtonTemplate")
-button:SetPoint("CENTER",0,0)
 button:SetWidth(width)
-button:SetHeight(height)
+button:SetHeight(28)
 button:SetText(text)
+button:SetPoint("CENTER",0, ((38 * buttonNo) - (parent:GetHeight() * 0.5) - 14))
 button:SetScript("OnClick", onClick) -- OnClick must exist before creating the button.
 return button
 end
 
+-- Creates a new input box.
+function newInputBox(width, parent, boxNo)
+local box = CreateFrame("EditBox", nil, parent, "InputBoxTemplate")
+box:SetWidth(width)
+box:SetHeight(28)
+box:SetPoint("CENTER",0, ((38 * boxNo) - (parent:GetHeight() * 0.5) - 14))
+local function onEnter()
+box:ClearFocus()
+end
+box:SetScript("OnEnterPressed", onEnter) -- onEnter must exist before creating the button.
+return box
+end
+
+-- Creates a new drop down menu.
+function newDropDown(width, items, parent, boxNo)
+if not DropDownMenuTest then
+   CreateFrame("Button", "DropDownMenuTest", parent, "UIDropDownMenuTemplate")
+end
+ 
+DropDownMenuTest:ClearAllPoints()
+DropDownMenuTest:SetPoint("CENTER", 0, ((38 * boxNo) - (parent:GetHeight() * 0.5) - 14))
+DropDownMenuTest:Show()
+ 
+local function OnClick(self)
+   UIDropDownMenu_SetSelectedID(DropDownMenuTest, self:GetID())
+end
+ 
+local function initialize(self, level)
+   local info = UIDropDownMenu_CreateInfo()
+   for k,v in pairs(items) do
+      info = UIDropDownMenu_CreateInfo()
+      info.text = v
+      info.value = v
+      info.func = OnClick
+      UIDropDownMenu_AddButton(info, level)
+   end
+end
+ 
+ 
+UIDropDownMenu_Initialize(DropDownMenuTest, initialize)
+UIDropDownMenu_SetWidth(DropDownMenuTest, 100);
+UIDropDownMenu_SetButtonWidth(DropDownMenuTest, 124)
+UIDropDownMenu_SetSelectedID(DropDownMenuTest, 1)
+UIDropDownMenu_JustifyText(DropDownMenuTest, "LEFT")
+return dropDown
+end
+
+-- Creates a new text frame.
+function newText(width, boxText, parent, boxNo)
+local text = CreateFrame("Frame","title",parent)
+text:SetHeight(28)
+text:SetWidth(width)
+text.text = text:CreateFontString(nil,nil, "GameFontNormal")
+text.text:SetAllPoints()
+text:SetPoint("CENTER", 0, ((38 * boxNo) - (parent:GetHeight() * 0.5) - 14))
+text.text:SetText(boxText);
+text:Show()
+end
+
+-- Creates a new slider.
+function newSlider(width, minValue, maxValue, initial, parent, boxNo)
+local slider = CreateFrame("Slider", nil, parent, "OptionsSliderTemplate")
+slider:SetMinMaxValues(minValue, maxValue)
+slider:SetWidth(width)
+slider:SetHeight(28)
+slider:SetPoint("CENTER",0, ((38 * boxNo) - (parent:GetHeight() * 0.5) - 14))
+slider:SetValue(initial)
+local text = CreateFrame("Frame","title",slider)
+text:SetHeight(28)
+text:SetWidth(width)
+text.text = text:CreateFontString(nil,nil, "GameFontNormal")
+text.text:SetAllPoints()
+text:SetPoint("CENTER", 0, 0)
+text.text:SetText(round(slider:GetValue()));
+text:Show()
+local function onSliderUpdate()
+text.text:SetText(round(slider:GetValue()));
+text:Show()
+end
+slider:SetScript("OnValueChanged", onSliderUpdate)
+return slider
+end
 print("frameTemplate loaded.")
--- main.lua
+++ main.lua
@@ -1,6 +1,45 @@
 -- The primary scripting file.
 
-local frame = newFrame("RPCombat",180,80)
-local button = newButton("Create encounter", 120, 28, frame, nil)
-print("main loaded.")
+-- Take turn
 
+-- DM Encounter screen
+
+-- Add Mobs
+function addMob(previousFrame)
+previousFrame:Hide()
+local frame = newFrame("Add Mob",250,8)
+rpcMobs = {}
+local inputBoxText = newText(210, "Mob name", frame, 8)
+local inputBox = newInputBox(210, frame, 7)
+local mobDiffText = newText(210, "Mob difficulty", frame, 6)
+local mobDiff = newSlider(210, 1, 20, 10, frame, 5)
+local mobRTText = newText(210, "Raid target icon", frame, 4)
+local mobRT = newDropDown(210, {"Skull", "Cross", "Circle", "Star", "Square", "Triangle", "Diamond", "Moon"}, frame, 3)
+local save = newButton("Save", 210, frame, nil, 2)
+local save = newButton("Cancel", 210, frame, nil, 1)
+end
+
+-- Create encounter
+function createEncounter(previousFrame)
+previousFrame:Hide()
+local frame = newFrame("Create Encounter",180,2)
+local function clickStart()
+end
+local startButton = newButton("Start encounter", 120, frame, nil, 1) -- TODO: Set the function for this.
+local function clickAddMob()
+addMob(frame)
+end
+local addMobButton = newButton("Add Mob", 120, frame, clickAddMob, 2)
+
+end
+-- Command
+SLASH_RPC1 = '/rpc'
+function SlashCmdList.RPC(msg, editbox)
+local frame = newFrame("RPCombat", 180, 1)
+local function click()
+createEncounter(frame)
+end
+local button = newButton("Create encounter", 120, frame, click, 1)
+end
+
+print("main loaded.")
