d56a6d0d06785104e17b8327973d69bbf919e7bb
Elijah Andrews <norman.reggox@gmail.com>
2014-12-19 10:46:47 +0000

    Continued development of mob saving

--- main.lua
+++ main.lua
@@ -8,15 +8,24 @@
 function addMob(previousFrame)
 previousFrame:Hide()
 local frame = newFrame("Add Mob",250,8)
-rpcMobs = {}
+local function returnToCreate()
+createEncounter(frame))
+end
+rpcMobDiffs = {}
+rpcMobRTs = {}
 local inputBoxText = newText(210, "Mob name", frame, 8)
 local inputBox = newInputBox(210, frame, 7)
 local mobDiffText = newText(210, "Mob difficulty", frame, 6)
 local mobDiff = newSlider(210, 1, 20, 10, frame, 5)
 local mobRTText = newText(210, "Raid target icon", frame, 4)
 local mobRT = newDropDown(210, {"Skull", "Cross", "Circle", "Star", "Square", "Triangle", "Diamond", "Moon"}, frame, 3)
-local save = newButton("Save", 210, frame, nil, 2)
-local save = newButton("Cancel", 210, frame, nil, 1)
+local function saveMob()
+local mobName = inputBoxText:GetText()
+rpcMobDiffs[mobName] = round(slider:GetValue())
+rpcMobRTs[mobName] = --TODO
+end
+local save = newButton("Save", 210, frame, saveMob, 2)
+local cancel = newButton("Cancel", 210, frame, returnToCreate, 1)
 end
 
 -- Create encounter
